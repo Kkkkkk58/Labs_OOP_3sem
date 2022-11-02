3ebed24029feca7cf2ba38cdd258274306ac30ec
Karim Khasan <karimhasan3879@gmail.com>
2022-11-02 18:05:45 +0300

    test: remove local fs occurrences

--- Lab3/Backups.Test/BackupsTest.cs
+++ Lab3/Backups.Test/BackupsTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿using Backups.Algorithms.Abstractions;
 using Backups.Models;
 using Backups.Models.Abstractions;
 using Backups.Models.Algorithms;
@@ -12,16 +12,17 @@ namespace Backups.Test;
 public class BackupsTest
 {
     [Fact]
-    public void BasicTest()
+    public void SplitAlgorithmUntrackObject_StorageNumberNotEqualsRestorePointsNumber()
     {
         var fs = new MemoryFileSystem();
         SeedFileSystem(fs);
 
         IRepository repository = new InMemoryRepository(fs, "/test");
-
         var bo = new BackupObject(repository, new InMemoryRepositoryAccessKey("input/test.txt"));
         var bo2 = new BackupObject(repository, new InMemoryRepositoryAccessKey("/input/dir"));
-        IBackupTask backupTask = GetBackupTask(repository);
+        var algorithm = new SplitStorageAlgorithm(Storage.Builder, ObjectStorageRelation.Builder);
+        IBackupTask backupTask = GetBackupTask(repository, algorithm);
+
         backupTask.TrackBackupObject(bo);
         backupTask.TrackBackupObject(bo2);
 
@@ -32,6 +33,30 @@ public class BackupsTest
 
         Assert.Equal(2, GetRestorePointsNumber(backupTask));
         Assert.Equal(3, GetStorageNumber(backupTask));
+    }
+
+    [Fact]
+    public void SingleAlgorithmUntrackObject_StorageNumberNotEqualsRestorePointsNumber()
+    {
+        var fs = new MemoryFileSystem();
+        SeedFileSystem(fs);
+
+        IRepository repository = new InMemoryRepository(fs, "/test");
+
+        var bo = new BackupObject(repository, new InMemoryRepositoryAccessKey("input/test.txt"));
+        var bo2 = new BackupObject(repository, new InMemoryRepositoryAccessKey("/input/dir"));
+        var algorithm = new SingleStorageAlgorithm(Storage.Builder, ObjectStorageRelation.Builder);
+        IBackupTask backupTask = GetBackupTask(repository, algorithm);
+        backupTask.TrackBackupObject(bo);
+        backupTask.TrackBackupObject(bo2);
+
+        backupTask.CreateRestorePoint();
+
+        backupTask.UntrackBackupObject(bo2);
+        backupTask.CreateRestorePoint();
+
+        Assert.Equal(2, GetRestorePointsNumber(backupTask));
+        Assert.Equal(2, GetStorageNumber(backupTask));
     }
 
     private static int GetRestorePointsNumber(IBackupTask backupTask)
@@ -47,13 +72,14 @@ public class BackupsTest
         return backupTask
             .Backup
             .RestorePoints
-            .Select(bt => bt.ObjectStorageRelations.Count)
-            .Sum();
+            .SelectMany(bt => bt.ObjectStorageRelations.Select(r => r.Storage))
+            .Distinct()
+            .Count();
     }
 
-    private static IBackupTask GetBackupTask(IRepository repository)
+    private static IBackupTask GetBackupTask(IRepository repository, IStorageAlgorithm algorithm)
     {
-        IBackupTaskConfiguration config = GetBackupTaskConfig(repository);
+        IBackupTaskConfiguration config = GetBackupTaskConfig(repository, algorithm);
 
         return new BackupTaskBuilder()
             .SetConfiguration(config)
@@ -61,10 +87,8 @@ public class BackupsTest
             .Build();
     }
 
-    private static IBackupTaskConfiguration GetBackupTaskConfig(IRepository repository)
+    private static IBackupTaskConfiguration GetBackupTaskConfig(IRepository repository, IStorageAlgorithm algorithm)
     {
-        var algorithm = new SplitStorageAlgorithm(Storage.Builder, ObjectStorageRelation.Builder);
-
         return new BackupTaskConfigurationBuilder()
             .SetTargetRepository(repository)
             .SetStorageAlgorithm(algorithm)
@@ -73,18 +97,33 @@ public class BackupsTest
 
     private static void SeedFileSystem(IFileSystem fs)
     {
+        using Stream data = GetData();
         fs.CreateDirectory("/test/input");
-        using Stream stream = fs.CreateFile("/test/input/test.txt");
-        {
-            using Stream another = File.OpenRead("D:/test/1.txt");
-            another.CopyTo(stream);
-        }
+        WriteData(data, fs.CreateFile("/test/input/test.txt"));
 
         fs.CreateDirectory("/test/input/dir");
-        using Stream anotherStream = fs.CreateFile("/test/input/dir/suren.txt");
+        WriteData(data, fs.CreateFile("/test/input/dir/suren.txt"));
+
+        WriteData(data, File.OpenWrite("out.txt"));
+    }
+
+    private static Stream GetData()
+    {
+        const int size = 2000;
+        byte[] data = Enumerable
+            .Range(0, size)
+            .Select(x => (byte)x)
+            .ToArray();
+
+        return new MemoryStream(data);
+    }
+
+    private static void WriteData(Stream inputStream, Stream outputStream)
+    {
+        using (outputStream)
         {
-            using var ms = new MemoryStream(Encoding.UTF8.GetBytes("TEST MESSAGE"));
-            ms.CopyTo(anotherStream);
+            inputStream.Seek(0, SeekOrigin.Begin);
+            inputStream.CopyTo(outputStream);
         }
     }
 }
